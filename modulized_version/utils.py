c98aff487f3ef4b07be8a021b939f38ddbf5c310
HAO YUAN <haoyuan@HAOs-MacBook-Pro.local>
2018-01-14 18:27:29 -0800

    correct one-hot

--- modulized_version/utils.py
+++ modulized_version/utils.py
@@ -1,8 +1,8 @@
-def one_hot(x, N = 24):
-    l = np.zeros(24)
+def one_hot(x, N):
+    l = np.zeros(N)
     l[x] = 1
     return l
 
-def inv_one_hot(l, N = 24):
+def inv_one_hot(l):
     return np.argmax(l)
 
